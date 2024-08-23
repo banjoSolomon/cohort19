2423e3790b97c4b522d3693386882a3e284ed93a
banjoSolomon <ayomidebanjo68@gmail.com>
2024-08-23 07:41:57 +0100

    update

---
+++ src/test/java/com/solo/devups/DevupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solo.devups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
